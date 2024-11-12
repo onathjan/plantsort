c32841c239a742d5deb75c8556134fb09fbde216
Jonathan Christian <christianj707@gmail.com>
2024-11-12 14:53:05 -0800

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  has_secure_password
+  has_many :sessions, dependent: :destroy
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email_address, presence: true, length: { maximum: 50 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+  normalizes :email_address, with: ->(e) { e.strip.downcase }
+end
