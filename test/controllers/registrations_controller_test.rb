5b3663f5670cd12e75ff27fa71031d4226480020
Jonathan Christian <christianj707@gmail.com>
2024-11-13 12:32:45 -0800

    add registration controller test

--- test/controllers/registrations_controller_test.rb
+++ test/controllers/registrations_controller_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class RegistrationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get new" do
+    get signup_path
+    assert_response :success
+    assert_select "title", "Sign Up | PlantSort"
+  end
 end
