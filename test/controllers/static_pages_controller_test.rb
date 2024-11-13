a9b4a30cf2457fe0509c57fb9b845cf00f842972
Jonathan Christian <christianj707@gmail.com>
2024-11-13 09:07:05 -0800

    add tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,7 +2,26 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
+    assert_select "title", "PlantSort"
+  end
+
+  test "should get terms of use" do
+    get terms_of_use_path
+    assert_response :success
+    assert_select "title", "Terms of Use | PlantSort"
+  end
+
+  test "should get privacy policy" do
+    get privacy_policy_path
+    assert_response :success
+    assert_select "title", "Privacy Policy | PlantSort"
+  end
+
+  test "should get password reset" do
+    get password_reset_path
+    assert_response :success
+    assert_select "title", "Password Reset | PlantSort"
   end
 end
