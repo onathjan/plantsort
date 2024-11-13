63774fff5686fb1c53ceb1708c5f3228e4a4b144
Jonathan Christian <christianj707@gmail.com>
2024-11-12 20:02:22 -0800

    add terms and privacy pages

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,15 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
 
-   root "static_pages#home"
+  root "static_pages#home"
+
+  get "terms-of-use",   to: "static_pages#terms"
+  get "privacy-policy", to: "static_pages#privacy"
 
   get "signup",   to: "registrations#new"
   post "signup",  to: "registrations#create"
