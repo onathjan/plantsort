04c24e8d81571c5278b7b9091e0d9e19833e13ab
Jonathan Christian <christianj707@gmail.com>
2024-11-14 19:06:29 -0800

    add category_ids requirement to test

--- test/models/plant_test.rb
+++ test/models/plant_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class PlantTest < ActiveSupport::TestCase
   def setup
-    @plant = Plant.new(name: "Dandelion", user_id: 1)
+    @plant = Plant.new(name: "Dandelion", user_id: 1, category_ids: [ 1 ])
     assert @plant.valid?
   end
 
@@ -17,6 +17,11 @@ class PlantTest < ActiveSupport::TestCase
 
   test "name can't be too long" do
     @plant.name = "a" * 26
+    assert_not @plant.valid?
+  end
+
+  test "shouldn't be valid without category ids" do
+    @plant.category_ids = nil
     assert_not @plant.valid?
   end
 end
