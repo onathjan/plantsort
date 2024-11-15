a111c1292e073dcf7d271e4f186e60e9f14bd728
Jonathan Christian <christianj707@gmail.com>
2024-11-14 16:30:30 -0800

    edit routes to get rid of unneeded actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "static_pages/home"
-  resource :session
+  resource :session, except: [ :new ]
 
   # keep commented out unless password mailers are needed
   # resources :passwords, param: :token
@@ -27,6 +27,6 @@ Rails.application.routes.draw do
   post "signup",  to: "registrations#create"
   get "login",    to: "sessions#new"
   get "settings", to: "registrations#edit"
-  resource :registrations
+  resource :registrations, except: [ :index, :show ]
   resources :plants, except: [ :index, :show ]
 end
