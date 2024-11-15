e21acb43183fdf989c93d582a8d0978d5ad33dcf
Jonathan Christian <christianj707@gmail.com>
2024-11-14 18:34:45 -0800

    add plants controller test

--- test/controllers/plants_controller_test.rb
+++ test/controllers/plants_controller_test.rb
@@ -1,7 +1,37 @@
 require "test_helper"
 
 class PlantsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:john)
+    @plant = plants(:one)
+    @category = categories(:one)
+    get login_path
+    post session_path(email_address: @user.email_address, password: "foobar")
+  end
+  test "user should be able to create a new plant" do
+    assert_difference "Plant.count", 1 do
+      get new_plant_path
+      assert_response :success
+      post plants_path(@plant.id), params: { plant: { name: "Green Bean", user_id: @user.id, category_ids: [ @category.id ] } }
+      assert_redirected_to root_url
+    end
+  end
+
+  test "user should be able to edit a plant" do
+    get edit_plant_path(@plant.id)
+    assert_response :success
+    patch plant_path(@plant.id), params: { plant: { name: "Something different", category_ids: [ @category.id ] } }
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_select "title", "PlantSort"
+    @plant.reload
+    assert_equal "Something different", @plant.name
+  end
+
+  test "user should be able to delete a plant" do
+    assert_difference "Plant.count", -1 do
+      delete plant_path(@plant.id)
+      assert_redirected_to root_path
+    end
+  end
 end
--- test/controllers/registrations_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-class RegistrationsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get signup_path
-    assert_response :success
-    assert_select "title", "Sign Up | PlantSort"
-  end
-end
