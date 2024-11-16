8440aa5d435bde4301786848d6524a4c6f3a9a94
Jonathan Christian <christianj707@gmail.com>
2024-11-16 10:38:56 -0800

    get basic sorting functionality working

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,14 @@
 class StaticPagesController < ApplicationController
   allow_unauthenticated_access
+
   def home
     if authenticated?
-      @plants = Current.user.plants.order(created_at: :desc)
       @categories = Category.all
+      @plants = filter_plants
+
+      if request.headers["Turbo-Frame"]
+        @plants
+      end
     end
   end
 
@@ -15,4 +20,18 @@ class StaticPagesController < ApplicationController
 
   def password_reset
   end
+
+  private
+
+    def filter_plants
+      if params[:category].present? && valid_category?(params[:category])
+        Current.user.plants.joins(:categories).where(categories: { name: params[:category] }).order(created_at: :desc)
+      else
+        Current.user.plants.order(created_at: :desc)
+      end
+    end
+
+    def valid_category?(category)
+      Category.exists?(name: category)
+    end
 end
