b000257bc851741a3c2ea390de2f1918a0a326d4
Jonathan Christian <christianj707@gmail.com>
2024-11-20 14:25:37 -0800

    add auto-timeout to notifications

--- app/javascript/controllers/flash_message_controller.js
+++ app/javascript/controllers/flash_message_controller.js
@@ -1,12 +1,26 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   static targets = ["notification"];
 
-  close(event) {
-    const notification = event.target.closest(".notification");
-    if (notification) {
-      notification.remove();
+  connect() {
+    // Automatically close the notification after 5 seconds
+    this.timeoutId = setTimeout(() => {
+      this.closeNotification();
+    }, 4000);
+  }
+
+  close() {
+    // Cancel the auto-close timeout to prevent conflicts
+    clearTimeout(this.timeoutId);
+
+    // Close the notification when the close button is clicked
+    this.closeNotification();
+  }
+
+  closeNotification() {
+    if (this.hasNotificationTarget) {
+      this.notificationTarget.remove();
     }
   }
 }
