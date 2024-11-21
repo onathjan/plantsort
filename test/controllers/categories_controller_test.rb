95bacb198300d243484960a0958b0c80429abb6f
Jonathan Christian <christianj707@gmail.com>
2024-11-21 11:43:24 -0800

    add categories controller tests

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,7 +1,43 @@
 require "test_helper"
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:john)
+    @category = categories(:one)
+    get login_path
+    post session_path(email_address: @user.email_address, password: "foobar")
+  end
+
+  test "should return index page" do
+    get categories_path
+    assert_response :success
+  end
+
+  test "should be able to create new category" do
+    assert_difference "Category.count", 1 do
+      get new_category_path
+      post categories_path(category: { name: "Test category" })
+      assert_redirected_to root_path
+      follow_redirect!
+      assert_select "title", "PlantSort"
+    end
+  end
+
+  test "should be able to update a category" do
+    get edit_category_path(@category.id)
+    assert_response :success
+    patch category_path(@category.id), params: { category: { name: "Something different" } }
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_select "title", "PlantSort"
+    @category.reload
+    assert_equal "Something different", @category.name
+  end
+
+  test "should be able to delete a category" do
+    assert_difference "Category.count", -1 do
+      delete category_path(@category.id)
+      assert_redirected_to root_path
+    end
+  end
 end
