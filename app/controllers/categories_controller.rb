ff9e8b78f6ad6e69b526aa525b3fe9f79495d20d
Jonathan Christian <christianj707@gmail.com>
2024-11-20 17:17:39 -0800

    create basic category controller

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,36 @@
+class CategoriesController < ApplicationController
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Current.user.categories.new(category_params)
+
+    if @category.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to root_path
+  end
+
+  private
+    def category_params
+      endparams.expect(category: [ :name, :user_id ])
+    end
+end
