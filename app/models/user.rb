e1cba5d64743bd03c04e5b5f1a4c2c37c0ddedcd
Jonathan Christian <christianj707@gmail.com>
2024-11-20 19:25:49 -0800

    add default categories to each user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   has_many :plants,     dependent: :destroy
   has_many :categories, dependent: :destroy
 
+  after_create :add_default_categories
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :email_address, presence: true, length: { minimum: 8, maximum: 50 },
@@ -12,4 +14,12 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
+
+  private
+    def add_default_categories
+      default_categories = Category.where(user_id: nil)
+      default_categories.each do |category|
+        self.categories.create(name: category.name)
+      end
+    end
 end
